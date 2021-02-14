563bc50dc6f327881e77296db2a7f8d6fe67b82f
Kayla Conceicao <Kayla.Conceicao@kineo.com>
2021-02-14 21:51:29 +0000

    Fisrt commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,10 @@ import React from "react";
 
 // Pages / Components
 import AboutMe from "./Pages/AboutMe";
-import Footer from "./Components/Footer"
-// import Contact from "./Pages/Contact";
+import Footer from "./Components/Footer";
 
 // Page transition
 import { AnimatePresence } from "framer-motion";
-
-// Animation
-// import pageAnimation from "./Animation";
 
 // Styles
 import GlobalStyle from "./Components/GlobalStyle";
