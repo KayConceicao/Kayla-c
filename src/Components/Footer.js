360b2106aa8773ab89a74d795bf5e9967ac7fe4c
Kayla Conceicao <kaylac@hotmail.co.uk>
2021-06-25 21:07:32 +0100

    contact form added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import EmailLink from "./EmailLink";
 
 
 // Icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedinIn, faInstagram, faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
-
-
 
   return (
     <div className="mainFooter">
@@ -20,7 +16,6 @@ const Footer = () => {
         <Link to={{pathname: "http://www.linkedin.com/in/kayla-conceicao-081813107"}} target="_blank"><FontAwesomeIcon className="contactIcon" icon={faLinkedinIn} /></Link>
         <Link to={{pathname: "https://www.instagram.com/kk_lah97/"}} target="_blank"><FontAwesomeIcon className="contactIcon" icon={faInstagram} /></Link>
         <Link to={{pathname: "https://github.com/KayConceicao"}} target="_blank"><FontAwesomeIcon className="contactIcon" icon={faGithub} /></Link>
-        <EmailLink label={[<FontAwesomeIcon className='contactIcon' icon={faEnvelope} />]} />
       </div>
     </div>
   );
--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,11 +1,78 @@
-import React from "react";
-import EmailLink from "../Components/EmailLink";
+import React, {useState} from "react";
+import { useForm } from "react-hook-form";
+import{ init, sendForm } from "emailjs-com";
+init("user_CYgU2VqH715rOw1BOp8oP");
 
 const Contact = () => {
+  const [contactNumber, setContactNumber] = useState("000000");
+  const [statusMessage, setStatusMessage] = useState("Message");
+  
+  const generateContactNumber = () => {
+    const numStr = "000000" + (Math.random() * 1000000 | 0);
+    setContactNumber(numStr.substring(numStr.length - 6));
+  }
+  
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  
+  const onSubmit = (data) => {
+    const statusMessage = document.querySelector('.status-message');
+    const form = document.querySelector('#contact-form');
+    generateContactNumber();
+    sendForm('default_service', 'template_8ydi7a', '#contact-form')
+      .then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+        form.reset();
+        setStatusMessage("Sent!");
+        statusMessage.className = "status-message success";
+        setTimeout(()=> {
+          statusMessage.className = 'status-message'
+        }, 5000)
+      }, function(error) {
+        console.log('FAILED...', error);
+        setStatusMessage("Failed to send message! Please try again later.");
+        statusMessage.className = "status-message failure";
+        setTimeout(()=> {
+          statusMessage.className = 'status-message'
+        }, 5000)
+      });
+  }
+
+  const message = watch('message') || "";
+  const messageCharsLeft = 1500 - message.length;
+
   return (
     <div className="contact">
       <h3>Contact me</h3>
-      <p><EmailLink label="Email me">Email me</EmailLink> to learn more about my services or to see if I am aviliable to build your site.</p>
+      <p>Want to find out more? Fill out the form below to get in touch.</p>
+
+      <div className="form-container">
+        <form id='contact-form' onSubmit={handleSubmit(onSubmit)}>
+          
+          <input type='hidden' name='contact_number' value={contactNumber} />
+          
+          <div className="form-inner">
+            {errors.user_name && errors.user_name.type === "required" && ( <div role="alert">Name is required<br/></div> )}
+            <input type='text' name='user_name' placeholder='Name' maxLength='30' aria-invalid={errors.user_name ? "true" : "false"}   {...register('user_name', { required: true })}/>
+          </div>
+
+          <div className="form-inner">
+            {errors.user_email && errors.user_email.type === "required" && ( <div role="alert">Email is required<br/></div> )}
+            <input type='email' name='user_email' {...register('user_email', { required: true })} placeholder='Email' />
+          </div>
+
+          <div className="form-inner">
+            {errors.message && errors.message.type === "required" && ( <div role="alert">Name is required<br/></div> )}
+            <textarea name='message' {...register('message', { required: true })} placeholder='Message'/>
+            <p>{messageCharsLeft}</p>
+          </div>
+
+          <div className="btn-message">          
+            <input className="styled-btn" type='submit' value='Send' />
+            <p className="status-message">{statusMessage}</p>
+          </div>
+        </form>
+      </div>
+
     </div>
   );
 };
